c677dca1573111d0299fad4b9f01bf71f8ea404a
batmandarkside <batmandarkside@gmail.com>
2014-10-23 00:34:20 +0400

    first commit

---
+++ IOS_first/CollectionViewController.swift
@@ -0,0 +1,25 @@
+//
+//  CollectionViewController.swift
+//  IOS_first
+//
+//  Created by дмитрий кузнецов on 10/23/14.
+//  Copyright (c) 2014 дмитрий кузнецов. All rights reserved.
+//
+
+import Foundation
+
+
+class CollectionViewController {
+    
+            var imageArray = [String]()
+    
+    init(){
+        
+    
+        
+    }
+    
+    func collectionCount() -> Int{
+        return imageArray.count
+    }
+}
---
+++ IOS_first/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  IOS_first
+//
+//  Created by дмитрий кузнецов on 10/22/14.
+//  Copyright (c) 2014 дмитрий кузнецов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
