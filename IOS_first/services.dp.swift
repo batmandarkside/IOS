7836ea433ca9f20bf37c219b538365b717863100
batmandarkside <batmandarkside@gmail.com>
2014-11-02 14:16:57 +0300

    delete one submodule and parse json

--- IOS_first/services.dp.swift
+++ IOS_first/services.dp.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import SwiftyJSON
 import Alamofire
 
 
@@ -20,22 +21,24 @@ struct servicesDP {
         "hotel" : "358469",
         "ticket" : "777489209",
 		"DepartureId" : "6733",
-		"ArrivalId" : "1735",
-		"StartVoyageDate" : "2014-12-08",
+		"ArrivalId" : "18820",
+		"StartVoyageDate" : "2014-12-09",
 		"EndVoyageDate" : "2014-12-19",
 		"TicketClass" : "0",
 		"Adult" : "2",
-		"HotelId" : "358469",
-		"TicketId" : "777489209",
+		"HotelId" : "455514",
+		"TicketId" : "942032620",
 		//"AddFilter" : "true",
-		"_" : "1414855640197"
+		"_" : "1414925006760"
 	]
 
+    /*
+
+    */
 	
     static func getHotels(params : [String: AnyObject]? = paramDP, hotelSuccess: (data: AnyObject?)->(), hotelError: ()->(), complete: ()->()) {
 		
         Alamofire.request(.GET, "http://www.lh.ios.inna.ru/api/v1/Packages/SearchHotels", parameters: params)
-            .validate()
 			.responseJSON { (request, response, data, error) in
 				
                 
@@ -45,7 +48,10 @@ struct servicesDP {
                     hotelError()
                 } else {
 
-                    println(response, "response")
+                    var jsonParse = data as NSDictionary
+                    var responseJSON = JSON(jsonParse)
+
+                    println(responseJSON["Filters"]["Tickets"])
 
                     println("===============")
                     //hotelSuccess(data: data?)
@@ -56,7 +62,8 @@ struct servicesDP {
 		}
 	}
 	
-	static func getTickets(param : [String: AnyObject]? = paramDP, hotelSuccess: ()->(), hotelError: ()->(), complete: ()->()){
+	static func getTickets(param : [String: AnyObject]? = paramDP, hotelSuccess: ()->(), hotelError: ()->(),
+        complete: ()->()){
 		
         Alamofire.request(.GET, "http://www.lh.ios.inna.ru/api/v1/Packages/SearchTickets", parameters: param)
 			.responseJSON { (request, response, data, error) in
